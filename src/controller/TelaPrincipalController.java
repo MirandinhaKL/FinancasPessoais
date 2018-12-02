cbe4c828b3fbd8b79d4f9ff19ff922f5081f582d
Mirandinha <karine.lm@hotmail.com>
2018-12-02 18:22:39 -0200

    Tela principal carregando todos os dados exigidos do BD.

--- src/controller/TelaPrincipalController.java
+++ src/controller/TelaPrincipalController.java
@@ -7,21 +7,33 @@ package controller;
 
 import aplicacao.Main;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Categoria;
+import model.Movimentacao;
+import model.TipoDeMovimentacao;
+import model.dao.MovimentacaoDAO;
 
 public class TelaPrincipalController implements Initializable {
 
     private Main main;
     private Stage palco;
-    
-    
+    private ObservableList<Movimentacao> movimentacaoObservable;
+    private MovimentacaoDAO movimentacaoDAO;
+    private List<Movimentacao> listaDeMovimentacoes;
+
     @FXML
     private Label labelSaldoAtual;
 
@@ -35,19 +47,29 @@ public class TelaPrincipalController implements Initializable {
     private Label labelUltimaMovimentacao;
 
     @FXML
-    private ComboBox<?> comboBoxMes;
+    private ComboBox<String> comboBoxMes;
 
     @FXML
-    private TableColumn<?, ?> colunaData;
+    private TableView<Movimentacao> tabelaMovimentacao;
 
     @FXML
-    private TableColumn<?, ?> colunaTipo;
+    private TableColumn<Movimentacao, LocalDate> colunaData;
 
     @FXML
-    private TableColumn<?, ?> colunaCategoria;
+    private TableColumn<TipoDeMovimentacao, String> colunaTipo;
 
     @FXML
-    private TableColumn<?, ?> colunaValor;
+    private TableColumn<Categoria, String> colunaCategoria;
+
+    @FXML
+    private TableColumn<Movimentacao, Long> colunaValor;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        preencheComboBoxMes();
+        criaTabela();
+        carregarTabelaComDadosDoBanco();
+    }
 
     @FXML
     void handleButtonExcluirMovimentacao(ActionEvent event) {
@@ -73,13 +95,33 @@ public class TelaPrincipalController implements Initializable {
     void handleReceitaXdespesa(ActionEvent event) {
         System.out.println("Abre tela gráfico receita x despesa");
     }
+
+    public void preencheComboBoxMes() {
+        comboBoxMes.getItems().removeAll(comboBoxMes.getItems());
+        comboBoxMes.getItems().addAll("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+    }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    private void criaTabela() {
+        tabelaMovimentacao.setItems(movimentacaoObservable);
+        colunaData.setCellValueFactory(new PropertyValueFactory<>("data"));
+        colunaTipo.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+        colunaCategoria.setCellValueFactory(new PropertyValueFactory<>("categoria"));
+        colunaValor.setCellValueFactory(new PropertyValueFactory<>("valor"));
+    }
     
-     public void setMain(Main main) {
+     public void carregarTabelaComDadosDoBanco() {
+        movimentacaoDAO = new MovimentacaoDAO();
+        listaDeMovimentacoes = movimentacaoDAO.retornaListaDeMovimentacoes();
+        movimentacaoObservable = FXCollections.observableArrayList(listaDeMovimentacoes);
+        tabelaMovimentacao.setItems(movimentacaoObservable);
+        colunaData.setCellValueFactory(new PropertyValueFactory<>("data"));
+        colunaTipo.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+        colunaCategoria.setCellValueFactory(new PropertyValueFactory<>("categoria"));
+        colunaValor.setCellValueFactory(new PropertyValueFactory<>("valor"));
+    }
+
+    public void setMain(Main main) {
         this.main = main;
     }
 
